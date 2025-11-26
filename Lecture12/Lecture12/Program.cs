c2d9707063932d906b02b404dcc2f30dbda2466b
redwormik <redwormik@gmail.com>
2025-11-26 09:45:56 +0100

    Add Lecture12

---
+++ Lecture12/Lecture12/Program.cs
@@ -0,0 +1,98 @@
+﻿namespace Lecture12;
+
+
+public class Program
+{
+	public static string[] ReadLines(TextReader reader)
+	{
+		string[] lines = new string[10];
+		int count = 0;
+		string line;
+
+		// null - chybějící objekt
+		// když ReadLine vratí null, znamená to konec vstupu
+		while ((line = reader.ReadLine()) != null) {
+			if (count >= lines.Length) {
+				Array.Resize(ref lines, lines.Length * 2);
+			}
+			lines[count] = line;
+			count += 1;
+		}
+
+		Array.Resize(ref lines, count);
+		return lines;
+	}
+
+
+	public static string[] ReadFile(string fileName)
+	{
+		using (TextReader reader = new StreamReader(fileName)) {
+			return ReadLines(reader);
+		}
+	}
+
+
+	public static void WriteLines(TextWriter writer, string[] lines)
+	{
+		foreach (string line in lines) {
+			writer.WriteLine(line);
+		}
+	}
+
+
+	public static void WriteFile(string fileName, string[] lines, bool append = false)
+	{
+		// druhý parametr StreamWriter - přepsat soubor (false, výchozí), nebo psát na konec (true)
+		using (TextWriter writer = new StreamWriter(fileName, append: append)) {
+			WriteLines(writer, lines);
+		}
+	}
+
+
+	private static void Main(string[] args)
+	{
+		string baseDir = @"..\..\..\";
+
+		// https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/using-statement
+
+		// https://learn.microsoft.com/en-us/dotnet/api/system.io.textwriter
+		// https://learn.microsoft.com/en-us/dotnet/api/system.io.streamwriter
+		using (TextWriter writer = new StreamWriter(baseDir + "hello.txt")) {
+			writer.Write("Hello, ");
+			writer.WriteLine("world!");
+		}
+		// writer.WriteLine(); nelze - writer není vidět
+
+		// https://learn.microsoft.com/en-us/dotnet/api/system.io.textwriter
+		// https://learn.microsoft.com/en-us/dotnet/api/system.io.streamreader
+		using (TextReader reader = new StreamReader(baseDir + "hello.txt")) {
+			Console.WriteLine(reader.ReadLine());
+		}
+
+		// using je přibližně:
+		{
+			TextWriter writer = new StreamWriter(baseDir + "lorem.txt");
+			try {
+				writer.WriteLine("Lorem ipsum");
+				writer.WriteLine("Dolor sit amet");
+			} finally {
+				if (writer != null) {
+					writer.Dispose(); // zavře Writer, zapíše na disk
+				}
+			}
+			// writer.WriteLine("Foo"); // nelze - writer je zavřený
+		}
+
+		// Vytvoří soubor, pokud neexistuje
+		using (TextWriter writer = new StreamWriter(baseDir + "lines.txt", append: true)) {
+		}
+
+		string[] fileLines = ReadFile(baseDir + "lines.txt");
+		WriteLines(Console.Out, fileLines);
+
+		Console.WriteLine("Enter text lines, enter Ctrl + Z to end:");
+		string[] consoleLines = ReadLines(Console.In);
+		WriteFile(baseDir + "lines.txt", consoleLines);
+		WriteFile(baseDir + "linesAll.txt", consoleLines, append: true);
+	}
+}
---
+++ Lecture12/Lecture12Tests/ProgramTests.cs
@@ -0,0 +1,174 @@
+﻿namespace Lecture12.Tests;
+
+using Lecture12;
+
+
+[TestClass]
+public class ProgramTests
+{
+	[TestMethod]
+	public void ReadLinesTest()
+	{
+		string input = "First line\nSecond line\nThird line\n";
+		using TextReader reader = new StringReader(input);
+
+		string[] expected = ["First line", "Second line", "Third line"];
+		string[] actual = Program.ReadLines(reader);
+		CollectionAssert.AreEqual(expected, actual);
+	}
+
+
+	[TestMethod]
+	public void ReadLinesTestNoTrailing()
+	{
+		string input = "First line\nSecond line";
+		using TextReader reader = new StringReader(input);
+
+		string[] expected = ["First line", "Second line"];
+		string[] actual = Program.ReadLines(reader);
+		CollectionAssert.AreEqual(expected, actual);
+	}
+
+
+	[TestMethod]
+	public void ReadLinesTestEmpty()
+	{
+		string[] expected = [];
+		string[] actual = Program.ReadLines(TextReader.Null);
+		CollectionAssert.AreEqual(expected, actual);
+	}
+
+
+	[TestMethod]
+	public void ReadLinesTestEmptyString()
+	{
+		using TextReader reader = new StringReader("");
+
+		string[] expected = [];
+		string[] actual = Program.ReadLines(reader);
+		CollectionAssert.AreEqual(expected, actual);
+	}
+
+
+	[TestMethod]
+	public void ReadFileTest()
+	{
+		using (TextWriter writer = new StreamWriter("ReadFileTest.txt")) {
+			writer.Write("Line 1\nLine 2\nLine 3\n");
+		}
+
+		string[] expected = ["Line 1", "Line 2", "Line 3"];
+		string[] actual = Program.ReadFile("ReadFileTest.txt");
+		File.Delete("ReadFileTest.txt");
+
+		CollectionAssert.AreEqual(expected, actual);
+	}
+
+
+	[TestMethod]
+	public void ReadFileTestNonExisting()
+	{
+		Assert.ThrowsException<FileNotFoundException>(() => {
+			Program.ReadFile("NonExistingFile.txt");
+		});
+	}
+
+
+	[TestMethod]
+	public void WriteLinesTest()
+	{
+		string[] lines = ["Line 1", "Line 2", "Line 3"];
+		using TextWriter writer = new StringWriter();
+		Program.WriteLines(writer, lines);
+
+		string expected = $"Line 1{Environment.NewLine}Line 2{Environment.NewLine}Line 3{Environment.NewLine}";
+		string actual = writer.ToString();
+		Assert.AreEqual(expected, actual);
+	}
+
+
+	[TestMethod]
+	public void WriteLinesTestEmptyArray()
+	{
+		string[] lines = [];
+		using TextWriter writer = new StringWriter();
+		Program.WriteLines(writer, lines);
+
+		string expected = "";
+		string actual = writer.ToString();
+		Assert.AreEqual(expected, actual);
+	}
+
+
+	[TestMethod]
+	public void WriteLinesTestEmptyString()
+	{
+		string[] lines = [""];
+		using TextWriter writer = new StringWriter();
+		Program.WriteLines(writer, lines);
+
+		string expected = Environment.NewLine;
+		string actual = writer.ToString();
+		Assert.AreEqual(expected, actual);
+	}
+
+
+	[TestMethod]
+	public void WriteFileTest()
+	{
+		string[] lines = ["Line 1", "Line 2", "Line 3"];
+		Program.WriteFile("WriteFileTest.txt", lines);
+		
+		
+		string expected = $"Line 1{Environment.NewLine}Line 2{Environment.NewLine}Line 3{Environment.NewLine}";
+		string actual;
+		using (TextReader reader = new StreamReader("WriteFileTest.txt")) {
+  			actual = reader.ReadToEnd();
+		}
+		File.Delete("WriteFileTest.txt");
+
+		Assert.AreEqual(expected, actual);
+	}
+
+
+	[TestMethod]
+	public void WriteFileTestExisting()
+	{
+		using (TextWriter writer = new StreamWriter("WriteFileTestExisting.txt")) {
+			writer.Write($"Old line 1{Environment.NewLine}Old line 2{Environment.NewLine}");
+		}
+
+		string[] lines = ["Line 1", "Line 2", "Line 3"];
+		Program.WriteFile("WriteFileTestExisting.txt", lines);
+
+		string expected = $"Line 1{Environment.NewLine}Line 2{Environment.NewLine}Line 3{Environment.NewLine}";
+		string actual;
+		using (TextReader reader = new StreamReader("WriteFileTestExisting.txt")) {
+			actual = reader.ReadToEnd();
+		}
+		File.Delete("WriteFileTestExisting.txt");
+
+		Assert.AreEqual(expected, actual);
+	}
+
+
+	[TestMethod]
+	public void WriteFileTestAppend()
+	{
+		using (TextWriter writer = new StreamWriter("WriteFileTestAppend.txt")) {
+			writer.Write($"Old line 1{Environment.NewLine}Old line 2{Environment.NewLine}");
+		}
+
+		string[] lines = ["Line 1", "Line 2", "Line 3"];
+		Program.WriteFile("WriteFileTestAppend.txt", lines, append: true);
+
+		string expected = $"Old line 1{Environment.NewLine}Old line 2{Environment.NewLine}Line 1{Environment.NewLine}Line 2{Environment.NewLine}Line 3{Environment.NewLine}";
+		string actual;
+		using (TextReader reader = new StreamReader("WriteFileTestAppend.txt")) {
+			actual = reader.ReadToEnd();
+		}
+		File.Delete("WriteFileTestAppend.txt");
+
+		Assert.AreEqual(expected, actual);
+	}
+}
