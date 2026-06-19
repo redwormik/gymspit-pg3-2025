3e92979f48cf712a8baa1ce740cac62cb14e551f
Jakub Červenka <jakub.cervenka@netvor.co>
2026-06-19 06:13:40 +0200

    Add Lecture9 (only BubbleSort)

---
+++ Lecture9/Lecture9/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Lecture9;
+
+
+public class Program
+{
+	public static void BubbleSort(int[] array)
+	{
+		int n = array.Length;
+		for (int i = 0; i < n - 1; i++) {
+			bool swapped = false;
+
+			for (int j = 0; j < n - i - 1; j++) {
+				if (array[j] > array[j + 1]) {
+					// Swap array[j] and array[j+1]
+					int temp = array[j];
+					array[j] = array[j + 1];
+					array[j + 1] = temp;
+					swapped = true;
+				}
+			}
+
+			if (!swapped) {
+				break;
+			}
+		}
+	}
+
+
+	static void Main(string[] args)
+	{
+	}
+}
---
+++ Lecture9/Lecture9Test/BubbleSortTests.cs
@@ -0,0 +1,15 @@
+﻿namespace Lecture9.Tests;
+
+
+[TestClass]
+public sealed class BubbleSortTests
+{
+	[TestMethod]
+	public void TestBubbleSort()
+	{
+		int[] array = { 64, 34, 25, 12, 22, 11, 90 };
+		int[] expected = { 11, 12, 22, 25, 34, 64, 90 };
+		Program.BubbleSort(array);
+		CollectionAssert.AreEqual(expected, array);
+	}
+}
